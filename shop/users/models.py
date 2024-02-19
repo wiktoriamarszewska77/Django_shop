5658a0b07002a86e891544e05fd7bf1576fe6687
wiktoriamarszewska77 <wiktoria.marszewska@wp.pl>
2024-02-19 17:08:48 +0100

    feat:Add admin functionality

--- shop/users/models.py
+++ shop/users/models.py
@@ -5,8 +5,14 @@ class User(AbstractUser):
     phone = models.CharField(max_length=9)
     address = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.username
+
 class Company(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     nip = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.user.username
