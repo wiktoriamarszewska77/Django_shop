394e574d88773140fb4d7562764f902cbcf12d5c
wiktoriamarszewska77 <wiktoria.marszewska@wp.pl>
2024-02-19 17:23:23 +0100

    feat:Create registration panel for user and saving to the database

--- shop/users/views.py
+++ shop/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import User
+from .forms import RegisterForm
 
-# Create your views here.
+
+class RegisterView(View):
+    def get(self, request):
+        form = RegisterForm()
+        return render(request, template_name='register.html', context={'form': form})
+
+    def post(self, request):
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render('login')
+        else:
+            form = RegisterForm()
+        return render(request, template_name='register.html', context={'form': form})
+
