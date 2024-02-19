4a57fdc7048986824c8001f7cf62fef52e8c8a82
wiktoriamarszewska77 <wiktoria.marszewska@wp.pl>
2024-02-19 17:28:30 +0100

    feat:Create login panel for user

--- shop/users/views.py
+++ shop/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .models import User
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth import authenticate
 
 
 class RegisterView(View):
@@ -18,3 +19,19 @@ class RegisterView(View):
             form = RegisterForm()
         return render(request, template_name='register.html', context={'form': form})
 
+class LoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, template_name='login.html', context={'form': form})
+
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                return redirect('profile')
+        else:
+            form = LoginForm()
+        return render(request, template_name='login.html', context={'form': form})
