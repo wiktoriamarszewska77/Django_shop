f6143b6724b3f988db15f12a8d4efee7a8ae1f87
wiktoriamarszewska77 <wiktoria.marszewska@wp.pl>
2024-02-19 17:04:28 +0100

    feat:Create models user and company

--- shop/users/models.py
+++ shop/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    phone = models.CharField(max_length=9)
+    address = models.CharField(max_length=100)
+
+class Company(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    nip = models.CharField(max_length=10)
