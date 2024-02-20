3d06730dd6728476c7b9ad1b0807b4acfc234254
wiktoriamarszewska77 <wiktoria.marszewska@wp.pl>
2024-02-20 16:49:15 +0100

    feat:Add settings and files for media

--- shop/users/models.py
+++ shop/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     phone = models.CharField(max_length=9)
     address = models.CharField(max_length=100)
