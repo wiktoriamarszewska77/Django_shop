6ec68725a06bb9f298d2c8a730a17ae428d3ce83
wiktoriamarszewska77 <wiktoria.marszewska@wp.pl>
2024-02-26 18:49:30 +0100

    feat:Create list, detail and add product view

--- shop/products/urls.py
+++ shop/products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView, ProductCreateView
 
 
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='home')
+    path('', ProductListView.as_view(), name='home'),
+    path('product/<int:pk>', ProductDetailView.as_view(), name='product-detail'),
+    path('new/product', ProductCreateView.as_view(), name='add-product'),
     ]
--- shop/products/views.py
+++ shop/products/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Product
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 
@@ -9,5 +12,22 @@ class ProductListView(ListView):
     template_name = 'home.html'
     context_object_name = 'products'
     ordering = ['-data_added']
-    paginate_by = 2
+    paginate_by = 4
 
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product.html'
+
+class ProductCreateView(CreateView, LoginRequiredMixin):
+    model = Product
+    template_name = 'add_product.html'
+    fields = '__all__'
+
+    def form_valid(self, form):
+        form.instance.company = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        product = self.get_object()
+        return self.request.user == product.seller
