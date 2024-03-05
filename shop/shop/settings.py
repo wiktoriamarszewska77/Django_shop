327d20167fdf6dce6ef7c6886ca1f64bdc5da53b
wiktoriamarszewska77 <wiktoria.marszewska@wp.pl>
2024-03-05 15:09:05 +0100

    feat: Add context_processors

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap4',
     'products',
+    'basket',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'basket.context_processors.basket',
             ],
         },
     },
