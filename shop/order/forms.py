36328ea8b3580b4fcbe8188607564bbae2dbdbfc
wiktoriamarszewska77 <wiktoria.marszewska@wp.pl>
2024-03-11 12:53:32 +0100

    feat: Adding a form and saving to the database

--- shop/order/forms.py
+++ shop/order/forms.py
@@ -1,26 +1,29 @@
 from django import forms
 from .models import Order
 from django.core.validators import RegexValidator
-
+from basket.basket import Basket
 class OrderForm(forms.ModelForm):
-    first_name = forms.CharField(required=True, max_length=100)
-    last_name = forms.CharField(required=True, max_length=100)
-    phone = forms.CharField(required=True, validators=[RegexValidator(r'^\d{9}$')])
-    email = forms.EmailField(required=True)
-    postcode = forms.CharField(required=True, validators=[RegexValidator(r'^\d{2}-\d{3}$')])
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    street = forms.CharField(max_length=50)
+    city = forms.CharField(max_length=50)
+    postcode = forms.CharField(max_length=6)
+    phone = forms.CharField(max_length=9)
+    email = forms.EmailField()
 
     class Meta:
         model = Order
-        fields = ['first_name', 'last_name', 'street', 'city', 'postcode', 'phone', 'email']
+        fields = ['first_name', 'last_name', 'street', 'city', 'postcode', 'phone', 'email', 'item', 'seller']
+
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user', None)
         initial_fields = {
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'city': user.address,
-            'phone': user.phone,
-            'email': user.email
+            'first_name': user.first_name if user else '',
+            'last_name': user.last_name if user else '',
+            'city': user.address if user else '',
+            'phone': user.phone if user else '',
+            'email': user.email if user else '',
         }
         super(OrderForm, self).__init__(*args, **kwargs)
 
--- shop/order/views.py
+++ shop/order/views.py
@@ -5,15 +5,29 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from basket.basket import Basket
 from .models import Order
-from users.models import User
+from users.models import User, Company
+from products.models import Product
 
 
 @method_decorator(login_required, name='dispatch')
 class OrderView(View):
     def get(self, request):
         order_form = OrderForm(user=request.user)
-
         basket = Basket(request)
         return render(request, template_name='order.html', context={'order_form': order_form, 'basket': basket})
+
+
+    def post(self, request):
+        order_form = OrderForm(request.POST)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            order.buyer = request.user
+            order.save()  # Zapisz zamówienie do bazy danych
+            basket = Basket(request)
+            # basket.clear()  # Wyczyść koszyk po złożeniu zamówienia
+            return redirect('profile')  # Przekieruj użytkownika na stronę profilu po złożeniu zamówienia
+
+        return render(request, 'order.html', {'order_form': order_form})
+
 
 
