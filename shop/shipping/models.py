e2663757bd2492d8f26b240e7a5ef2f25102f265
wiktoriamarszewska77 <wiktoria.marszewska@wp.pl>
2024-03-14 13:03:34 +0100

    feat:Creating a model and adding objects to the database

--- shop/shipping/models.py
+++ shop/shipping/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Shipping(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    delivery_time = models.CharField(max_length=20)
+    free_delivery = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+    def __str__(self):
+        return f"{self.name}\nprice: {self.price}\nDelivery time: {self.delivery_time}\nFree delivery: {self.free_delivery}"
