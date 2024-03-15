3bc8ff335159908927244538305503e112d6cb80
wiktoriamarszewska77 <wiktoria.marszewska@wp.pl>
2024-03-15 14:04:18 +0100

    feat:Getting shpping cost

--- shop/shipping/models.py
+++ shop/shipping/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from basket.basket import Basket
 
 
 
@@ -8,6 +9,11 @@ class Shipping(models.Model):
     delivery_time = models.CharField(max_length=20)
     free_delivery = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def get_shipping_cost(self, basket_total):
+        if basket_total >= self.free_delivery:
+            return 'Free delivery'
+        else:
+            return self.price
 
     def __str__(self):
         return f"{self.name}\nprice: {self.price}\nDelivery time: {self.delivery_time}\nFree delivery: {self.free_delivery}"
