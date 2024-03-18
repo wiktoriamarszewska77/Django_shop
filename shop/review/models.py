4671a53f605b3b5983656a9e7ed8eb5823858721
wiktoriamarszewska77 <wiktoria.marszewska@wp.pl>
2024-03-18 16:38:24 +0100

    feat:Creating a review model

--- shop/review/models.py
+++ shop/review/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import User
+from products.models import Product
+from django.core.validators import MinValueValidator, MaxValueValidator
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    comment = models.TextField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 
-# Create your models here.
+    def __str__(self):
+        return f'Review by {self.user} for {self.product} and rating {self.rating}'
